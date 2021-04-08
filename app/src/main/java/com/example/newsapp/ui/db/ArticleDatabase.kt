753e0f98e8323bee05170a6713abf9a54a2848b2
AnkitJha2000 <68436565+AnkitJha2000@users.noreply.github.com>
2021-04-08 23:13:36 +0530

    news app

--- app/src/main/java/com/example/newsapp/ui/db/ArticleDatabase.kt
+++ app/src/main/java/com/example/newsapp/ui/db/ArticleDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.newsapp.ui.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.newsapp.ui.Article
 
@@ -14,6 +16,19 @@ abstract class ArticleDatabase : RoomDatabase(){
     abstract fun getArticlesDao() : ArticleDao
 
     companion object{
+        @Volatile
+        private var instance : ArticleDatabase? = null
+        private val LOCK = Any()
 
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase (context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDatabase :: class.java,
+                "article_db.db"
+            ).build()
     }
 }
---
+++ app/src/main/java/com/example/newsapp/ui/db/Converters.kt
@@ -0,0 +1,5 @@
+package com.example.newsapp.ui.db
+
+class Converters {
+
+}
